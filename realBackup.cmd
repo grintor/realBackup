6dc4cbf3da413719a7c129a015f55741685987f9
Chris Wheeler <grintor@gmail.com>
2016-05-04 09:56:09 -0400

    face lift

--- realBackup.cmd
+++ realBackup.cmd
@@ -1,31 +1,48 @@
 @echo off
-if "%2"=="" echo Usage: %~n0 "<full path to source>" "<full path to destination>" & exit /b 1
+if "%2"=="" goto usage
+if not "%3"=="" if "%5"=="" goto usage
+
+goto start
+:usage
+echo Usage:
+echo %~n0 ^<src_path^> ^<dest_path^> [gmailAddr] [enc_pwd] [recpt_addr] [prefix]
+exit /b 1
+
+----------------------------------------------------
+
+:start
 
 set source_path=%~1
 set destination_path=%~2
+set gmail_address=%~3
+set base-64_enc_password=%~4
+set recpt_addr=%~5
+set subject_prefix=%~6
 set drive_letter=%source_path:~0,2%
 set tmp_mount=%cd%\%random%
 set shadow_source_path=%tmp_mount%\%source_path:~3%
+if defined subject_prefix set subject_prefix=%subject_prefix%: 
+
+if not exist "%source_path%" goto fail6
 
 for /f "tokens=1,2,* delims=;= " %%a in ('wmic shadowcopy call create "ClientAccessible"^,"%drive_letter%\"') do set %%a=%%~b
-if not '%ReturnValue%'=='0' goto :fail1
+if not '%ReturnValue%'=='0' goto fail1
 
 for /f "tokens=2 delims=?" %%a in ('vssadmin list shadows /shadow^=%ShadowID% ^| find "\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy"') do set shadow_volume=\\?%%a\
-if '%shadow_volume%'=='' goto :fail2
+if not defined shadow_volume goto fail2
 
 mklink /d "%tmp_mount%" "%shadow_volume%" > nul
-if not %errorlevel%==0 goto :fail3
+if not %errorlevel%==0 goto fail3
 
 robocopy "%shadow_source_path%" "%destination_path%" /MIR /FFT > nul
-if %errorlevel% gtr 1 goto :fail4
+if %errorlevel% gtr 1 goto fail4
 
 robocopy "%shadow_source_path%" "%destination_path%" /E /Copy:S /IS /IT > nul
-if %errorlevel% gtr 1 goto :fail5
+if %errorlevel% gtr 1 goto fail5
 
 rd "%tmp_mount%"
 vssadmin delete shadows /shadow=%ShadowID% /quiet > nul
 
-::call :send_email %computername% "backup success"
 exit /b 0
 
 ----------------------------------------------------
@@ -33,51 +50,64 @@ exit /b 0
 :fail1
 set err=failed to create volume shadow copy
 echo %err%
-call :send_email %computername% "%err%"
+if defined gmail_address call :send_email "%err%"
 exit /b 100
+
+----------------
 
 :fail2
 set err=failed to get a shadow copy volume name
 echo %err%
-call :send_email %computername% "%err%"
+if defined gmail_address call :send_email "%err%"
 exit /b 200
+
+----------------
 
 :fail3
 set err=failed to create symbolic link to volume shadow copy
 echo %err%
-call :send_email %computername% "%err%"
-vssadmin delete shadows /shadow=%ShadowID% /quiet
+if defined gmail_address call :send_email "%err%"
+vssadmin delete shadows /shadow=%ShadowID% /quiet > nul
 exit /b 300
+
+----------------
 
 :fail4
 set err=failed to copy files to the destination
 echo %err%
-call :send_email %computername% "%err%"
+if defined gmail_address call :send_email "%err%"
 rd "%tmp_mount%"
-vssadmin delete shadows /shadow=%ShadowID% /quiet
+vssadmin delete shadows /shadow=%ShadowID% /quiet > nul
 exit /b 400
+
+----------------
 
 :fail5
 set err=failed to copy the file ACL information to the destination
 echo %err%
-call :send_email %computername% "%err%"
+if defined gmail_address call :send_email "%err%"
 rd "%tmp_mount%"
-vssadmin delete shadows /shadow=%ShadowID% /quiet
+vssadmin delete shadows /shadow=%ShadowID% /quiet > nul
 exit /b 500
 
-------------------------------------------------------
+----------------
+
+:fail6
+set err=the source does not exist
+echo %err%
+if defined gmail_address call :send_email "%err%"
+exit /b 600
+
+----------------------------------------------------
 
 :send_email
-set pwd=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("REDACTED=="))
-set subject=%~1
-set body=%~2
+set body=%~1
 
-echo $SMTPClient = New-Object Net.Mail.SmtpClient("smtp.gmail.com", 587); >> email.ps1
-echo $SMTPClient.EnableSsl = $true; >> email.ps1
-echo $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("alerts@georgiatc.com", %pwd%); >> email.ps1
-echo $SMTPClient.Send("alerts@georgiatc.com", "support@gmail.com", "%subject%", "%body%"); >> email.ps1
-PowerShell.exe -ExecutionPolicy ByPass -file "%cd%\email.ps1"
-del email.ps1
+powershell -command $SMTPClient = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587); ^
+                    $SMTPClient.EnableSsl = $true; ^
+                    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential('%gmail_address%', ^
+                       [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%base-64_enc_password%'))); ^
+                    $SMTPClient.Send('%gmail_address%', '%recpt_addr%', '%subject_prefix%%computername%', '%body%');
 goto :EOF
 
 
